fcb2f18604196179adf01dd99acd8319074b418a
SAGH <42285767+8sagh8@users.noreply.github.com>
2021-03-06 13:03:38 -0500

    changes in route URL and PATH

--- haq/urls.py
+++ haq/urls.py
@@ -16,15 +16,12 @@ urlpatterns = [
     path('languages/', views.LanguagesView, name="languages"),
     url(r'getLanguagesBooks/(?P<language_id>[0-9]+)/$', views.GetLanguagesBooksView, name="getLanguagesBooks"),
     path('categories/', views.CategoriesView, name="categories"),
-    # url(r'^getCategoriesBooks/(?P<category_id>[0-9]+)/$', views.GetCategoriesBooksView, name="getCategoriesBooks"),
-    path(r'^getCategoriesBooks/(?P<category_id>[0-9]+)/$', views.GetCategoriesBooksView, name="getCategoriesBooks"),
+    path(r'^getCategoriesBooks/(?P<category_id>\d+)/$', views.GetCategoriesBooksView, name="getCategoriesBooks"),
     path('topicSearch/', views.TopicSearchView, name="topicSearch"),
-    # url(r'^getTopic/(?P<topic_id>[0-9]+)/$', views.GetTopicView, name="getTopic"),
-    url(r'^getTopic/(?P<topic_id>[0-9]+)/', views.GetTopicView, name="getTopic"),
+    url(r'^getTopic/(?P<topic_id>\d+)/', views.GetTopicView, name="getTopic"),
     path('reference/', views.ReferenceView, name="reference"),
     path('personalities/', views.PersonalityView, name="personalities"),
-    # url(r'^getPersonRef/(?P<person_id>[0-9]+)/$', views.GetPersonRefView, name="getPersonRef"),
-    path(r'^getPersonRef/<int:person_id>/$', views.GetPersonRefView, name="getPersonRef"),
+    path(r'getPersonRef/<int:person_id>/', views.GetPersonRefView, name="getPersonRef"),
 
 
     path('', views.IndexView, name="index"),
@@ -46,5 +43,12 @@ urlpatterns = [
     # path('getTopic/', views.GetTopicView, name="getTopic"),
     path('personalitySearch/', views.PersonalitySearchView, name="personalitySearch"),
     path('osample', views.OSampleView, name="osample"),
+
+    #  other ways to write route
+    # url(r'^getPersonRef/(?P<person_id>[0-9]+)/$', views.GetPersonRefView, name="getPersonRef"),
+    # url(r'^getTopic/(?P<topic_id>[0-9]+)/$', views.GetTopicView, name="getTopic"),
+    # url(r'^getCategoriesBooks/(?P<category_id>[0-9]+)/$', views.GetCategoriesBooksView, name="getCategoriesBooks"),
+    
 ]
+
 
